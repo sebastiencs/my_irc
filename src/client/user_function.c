f827a3c53aa598aaf247ef173e13c75fe767ae41
chapui_s <chapui_s@epitech.eu>
2015-04-11 22:11:01 +0200

    ajout de la commande /users

--- src/client/user_function.c
+++ src/client/user_function.c
@@ -5,14 +5,19 @@
 ** Login   <cholet_v@epitech.net>
 **
 ** Started on  Fri Apr 10 01:25:17 2015 cholet_v
-** Last update Fri Apr 10 19:25:00 2015 cholet_v
+** Last update Sat Apr 11 22:10:26 2015 chapui_s
 */
 
 #include "client.h"
 
-int	send_users(t_client *client)
+int		send_users(t_client *client)
 {
-  (void)client;
+  char		*channel;
+
+  if (!(channel = client->channel[client->current_chan]))
+    return (derror("You're not in any channel"));
+  snprintf(client->buffer_out, 512, "NAMES %s", channel);
+  client->action = WRITE;
   printf("send_users\n");
   return (0);
 }
