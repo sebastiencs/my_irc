9b14cace76fb416cbf3f669e48a41c63e15ebb73
chapui_s <chapui_s@epitech.eu>
2015-04-11 21:34:45 +0200

    malloc(size + 1) + mise a la norme

--- src/client/join_function.c
+++ src/client/join_function.c
@@ -5,14 +5,15 @@
 ** Login   <cholet_v@epitech.net>
 **
 ** Started on  Fri Apr 10 01:23:37 2015 cholet_v
-** Last update Sat Apr 11 17:57:48 2015 cholet_v
+** Last update Sat Apr 11 21:34:04 2015 chapui_s
 */
 
 #include "client.h"
 
-int	send_join(t_client *client)
+int		send_join(t_client *client)
 {
-  int	i;
+  size_t	size;
+  int		i;
 
   i = 0;
   if (count_tab(client->tab) != 2)
@@ -21,8 +22,10 @@ int	send_join(t_client *client)
   client->action = WRITE;
   while (client->channel[i] != NULL && i <= 9)
     ++i;
-  if ((client->channel[i] = malloc(strlen(client->tab[1]) * sizeof(char))) == NULL)
+  size = strlen(client->tab[1]);
+  if (!(client->channel[i] = malloc(size + 1)))
     return (derror("malloc:"));
+  memset(client->channel[i], 0, size + 1);
   strcpy(client->channel[i], client->tab[1]);
   client->current_chan = i;
   printf("send_join\n  %d\n%s", i, client->channel[i]);
